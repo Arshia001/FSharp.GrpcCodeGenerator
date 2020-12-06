348680ab48f44c729c66e6f44d5666a42074b37c
Arshia Ghafoori <arshia.g@abramad.com>
2020-12-06 09:31:16 +0330

    Remove debug mode

--- FSharp.GrpcCodeGenerator/Program.fs
+++ FSharp.GrpcCodeGenerator/Program.fs
@@ -6,26 +6,9 @@ open Google.Protobuf.FSharp
 
 [<EntryPoint>]
 let main _ =
-    let debugMode =
-        //true
-        false
-    let req = 
-        if debugMode
-        then
-            let bytes = IO.File.ReadAllBytes "req.bin"
-            Compiler.CodeGeneratorRequest.Parser.ParseFrom(bytes)
-        else
-            use stdIn = Console.OpenStandardInput()
-            let req = Compiler.CodeGeneratorRequest.Parser.ParseFrom(stdIn)
-            IO.File.Delete "req.bin"
-            use file = IO.File.OpenWrite "req.bin"
-            req.WriteTo(file)
-            req
+    use stdIn = Console.OpenStandardInput()
+    let req = Compiler.CodeGeneratorRequest.Parser.ParseFrom(stdIn)
     let resp = CodeGenerator.generate req
-    if debugMode
-    then
-        IO.Directory.CreateDirectory "__DEBUG" |> ignore
-        resp.File |> Seq.iter (fun f -> IO.File.WriteAllText(IO.Path.Combine("__DEBUG", f.Name.Value), f.Content.Value))
     use stdOut = Console.OpenStandardOutput()
     resp.WriteTo(stdOut)
     0
